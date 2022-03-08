a005ace9d89894e4347ef8b2ba215c3534c9a1bc
Em <contactemilyhuang@gmail.com>
2022-03-07 21:27:52 -0800

    Add overflow behaviour and add space in project list

--- src/updateProjectList.js
+++ src/updateProjectList.js
@@ -1,6 +1,6 @@
 const updateProjectList = (project) =>{
     const projectLiEl = document.querySelector(`li[data-project-id="${project.id}"]`);
-    projectLiEl.textContent = `${project.icon}${project.title}`;
+    projectLiEl.textContent = `${project.icon}`+"\u00a0"+`${project.title}`;
 };
 
 export {updateProjectList};
